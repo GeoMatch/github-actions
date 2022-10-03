7eb123d76fa37bbece299c5e720350533920a3f6
Michael Maurer <mjmaurer777@gmail.com>
2022-10-02 20:24:03 -0400

    [feat] Add syncronous ECS run action

---
+++ actions/ecs-run-sync/index.js
@@ -0,0 +1,62 @@
+const core = require("@actions/core");
+import {
+  ECSClient,
+  RunTaskCommand,
+  waitUntilTasksStopped,
+} from "@aws-sdk/client-ecs";
+
+const runTaskSync = async (runTaskConfig, cmd) => {
+  let command = cmd;
+  if (!(command.startsWith("[") && command.endsWith("]"))) {
+    command = JSON.stringify(command.split(" "));
+  }
+
+  const ecsClient = new ECSClient({ region: runTaskConfig.AWS_REGION });
+  const runTaskCommand = new RunTaskCommand({
+    count: 1,
+    launchType: "FARGATE",
+    cluster: runTaskConfig.AWS_GEOMATCH_CLUSTER_ARN,
+    taskDefinition: runTaskConfig.AWS_GEOMATCH_TASK_DEF_ARN,
+    overrides: {
+      containerOverrides: [
+        {
+          name: runTaskConfig.AWS_GEOMATCH_ECS_CONTAINER_NAME,
+          command: command,
+        },
+      ],
+    },
+    networkConfiguration: {
+      awsvpcConfiguration: {
+        subnets: [runTaskConfig.AWS_GEOMATCH_TASK_SUBNET],
+        securityGroups: [runTaskConfig.AWS_GEOMATCH_TASK_SECURITY_GROUP],
+        assignPublicIp: "ENABLED",
+      },
+    },
+  });
+  const ecsTask = await ecsClient.send(runTaskCommand);
+  const taskArn = ecsTask.tasks[0].taskArn;
+  if (typeof taskArn !== "string") {
+    throw Error("Task ARN is not defined.");
+  }
+  const waitECSTask = await waitUntilTasksStopped(
+    { client: ecsClient, maxWaitTime: 600, maxDelay: 20, minDelay: 1 },
+    { cluster: runTaskConfig.AWS_GEOMATCH_CLUSTER_ARN, tasks: [taskArn] }
+  );
+  console.log(waitECSTask.state);
+  return taskArn;
+};
+
+const run = async () => {
+  try {
+    const config = core.getInput("ecs-run-task-config");
+    const taskArn = await runTaskSync(
+      JSON.parse(config),
+      core.getInput("command")
+    );
+    core.setOutput("task-arn", taskArn);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+run();
---
+++ actions/ssm-get-param/index.js
@@ -0,0 +1,22 @@
+const core = require("@actions/core");
+import { GetParameterCommand, SSMClient } from "@aws-sdk/client-ssm";
+
+const getSSMParam = async (path) => {
+  const ssmClient = new SSMClient();
+  const getCommand = new GetParameterCommand({ Name: path });
+  const getCommandResult = await ssmClient.send(getCommand);
+  return getCommandResult.Parameter.Value;
+};
+
+const run = async () => {
+  try {
+    const ssmPath = core.getInput("ssm-path");
+    const val = await getSSMParam(ssmPath);
+    console.log(val);
+    core.setOutput("value", val);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+run();
