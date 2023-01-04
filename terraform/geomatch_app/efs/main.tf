7131bdc386e4eb9b7ab244a20291b9c9782fbf6f
Michael Maurer <mjmaurer777@gmail.com>
2023-01-04 00:09:13 -0500

    fix policy

--- terraform/geomatch_app/efs/main.tf
+++ terraform/geomatch_app/efs/main.tf
@@ -28,7 +28,7 @@ resource "aws_efs_file_system" "this" {
 
 resource "aws_efs_file_system_policy" "this" {
   file_system_id                     = aws_efs_file_system.this.id
-  bypass_policy_lockout_safety_check = false
+  bypass_policy_lockout_safety_check = true
 
   # Deny any traffic that isn't secure by default
   policy = jsonencode(
