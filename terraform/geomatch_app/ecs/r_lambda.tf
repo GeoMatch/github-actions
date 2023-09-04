fa290dc45814618608436796754982c334456634
Michael Maurer <mjmaurer777@gmail.com>
2023-09-04 14:24:32 -0400

    [fix] Turn off renv cache

--- terraform/geomatch_app/ecs/r_lambda.tf
+++ terraform/geomatch_app/ecs/r_lambda.tf
@@ -63,10 +63,11 @@ resource "aws_lambda_function" "r_lambda" {
       APP_EFS_DIR        = local.app_efs_container_mount_path
       GEOMATCH_VERSION   = var.geomatch_version
       RENV_PATHS_LIBRARY = "renv/library"
-      # Lambda home dir isn't writable
-      RENV_PATHS_ROOT    = "${local.lambda_efs_mount_path}/.cache/R/renv"
-      RENV_PATHS_SANDBOX    = "${local.lambda_efs_mount_path}/.cache/R/renv/sandbox"
-    }
+      # Lambda home dir isn't writable.
+      # Fix: turned off cache entirely
+      # RENV_PATHS_ROOT    = "${local.lambda_efs_mount_path}/.cache/R/renv"
+      # RENV_PATHS_SANDBOX = "${local.lambda_efs_mount_path}/.cache/R/renv/sandbox"
+       }
   }
 
   vpc_config {
