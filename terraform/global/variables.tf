45b42f5429e08d38bf5cc3190a2e95ac0e5347f7
Michael Maurer <mjmaurer777@gmail.com>
2023-10-24 20:47:47 -0700

    Turn off SFTP server

--- terraform/global/variables.tf
+++ terraform/global/variables.tf
@@ -1,5 +1,5 @@
 variable "create_sftp_server" {
   description = "Flag to determine whether to create (or destroy) the SFTP server"
   type        = bool
-  default     = true
+  default     = false
 }
