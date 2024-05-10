e3546527497051c48d8285c72fe91e7d5bdfc613
Michael Maurer <mjmaurer777@gmail.com>
2024-05-10 10:57:15 -0400

    [refactor] Remove prod user from sftp module

--- terraform/geomatch_app/sftp/main.tf
+++ terraform/geomatch_app/sftp/main.tf
@@ -73,51 +73,6 @@ resource "aws_transfer_server" "this" {
   }
 }
 
-resource "aws_iam_role" "sftp_prod_user" {
-  name = "${var.project}-prod-sftp-user-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "transfer.amazonaws.com"
-        }
-      }
-    ]
-  })
-
-  inline_policy {
-    name = "${var.project}-prod-sftp-user-policy"
-    policy = jsonencode({
-      "Version" : "2012-10-17",
-      "Statement" : [
-        {
-          "Action" : [
-            "s3:ListBucket"
-          ],
-          "Effect" : "Allow",
-          "Resource" : ["${aws_s3_bucket.sftp_prod.arn}"]
-        },
-        {
-          # s3:GetObject is not allowed
-          "Action" : [
-            "s3:PutObject", "s3:DeleteObject"
-          ],
-          "Effect" : "Allow",
-          "Resource" : ["${aws_s3_bucket.sftp_prod.arn}/*"]
-        },
-      ]
-    })
-  }
-
-  tags = {
-    Project     = var.project
-    Environment = "prod"
-  }
-}
 
 resource "aws_iam_role" "sftp_staging_user" {
   name = "${var.project}-staging-sftp-user-role"
@@ -203,19 +158,6 @@ resource "aws_iam_role" "sftp_logging" {
   }
 }
 
-resource "aws_transfer_user" "sftp_prod" {
-  count          = var.sftp_server_up ? 1 : 0
-  server_id      = aws_transfer_server.this[0].id
-  role           = aws_iam_role.sftp_prod_user.arn
-  user_name      = data.aws_ssm_parameter.prod_sftp_username.value
-  home_directory = "/${aws_s3_bucket.sftp_prod.bucket}"
-
-  tags = {
-    Project     = var.project
-    Environment = "prod"
-  }
-}
-
 resource "aws_transfer_user" "sftp_staging" {
   count          = var.sftp_server_up ? 1 : 0
   server_id      = aws_transfer_server.this[0].id
@@ -229,27 +171,11 @@ resource "aws_transfer_user" "sftp_staging" {
   }
 }
 
-resource "aws_transfer_ssh_key" "sftp_prod" {
-  count     = var.sftp_server_up ? 1 : 0
-  server_id = aws_transfer_server.this[0].id
-  user_name = aws_transfer_user.sftp_prod[0].user_name
-  body      = data.aws_ssm_parameter.prod_user_public_key.value
-}
-
 resource "aws_transfer_ssh_key" "sftp_staging" {
   count     = var.sftp_server_up ? 1 : 0
   server_id = aws_transfer_server.this[0].id
   user_name = aws_transfer_user.sftp_staging[0].user_name
   body      = data.aws_ssm_parameter.staging_user_public_key.value
-}
-
-resource "aws_s3_bucket" "sftp_prod" {
-  bucket = "${var.project}-prod-sftp"
-
-  tags = {
-    Project     = var.project
-    Environment = "prod"
-  }
 }
 
 resource "aws_s3_bucket" "sftp_staging" {
@@ -261,13 +187,6 @@ resource "aws_s3_bucket" "sftp_staging" {
   }
 }
 
-resource "aws_s3_bucket_ownership_controls" "sftp_prod" {
-  bucket = aws_s3_bucket.sftp_prod.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
 resource "aws_s3_bucket_ownership_controls" "sftp_staging" {
   bucket = aws_s3_bucket.sftp_staging.id
   rule {
@@ -275,26 +194,11 @@ resource "aws_s3_bucket_ownership_controls" "sftp_staging" {
   }
 }
 
-resource "aws_s3_bucket_acl" "stfp_prod" {
-  bucket = aws_s3_bucket.sftp_prod.id
-  acl    = "private"
-
-  depends_on = [aws_s3_bucket_ownership_controls.sftp_prod]
-}
-
 resource "aws_s3_bucket_acl" "stfp_staging" {
   bucket = aws_s3_bucket.sftp_staging.id
   acl    = "private"
 
   depends_on = [aws_s3_bucket_ownership_controls.sftp_staging]
-}
-
-resource "aws_s3_bucket_versioning" "sftp_prod" {
-  bucket = aws_s3_bucket.sftp_prod.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
 }
 
 resource "aws_s3_bucket_versioning" "sftp_staging" {
