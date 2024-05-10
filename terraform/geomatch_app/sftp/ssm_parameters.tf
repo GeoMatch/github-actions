7e4b3da81b12be4d703aece0ba59d930388781d6
Michael Maurer <mjmaurer777@gmail.com>
2024-05-10 13:36:15 -0400

    [refactor] Remove staging transfer user resoruces (post-terraform state mv)

--- terraform/geomatch_app/sftp/ssm_parameters.tf
+++ terraform/geomatch_app/sftp/ssm_parameters.tf
@@ -1,34 +1,7 @@
 locals {
-  ssm_name_prefix                  = "/${var.project}"
-  ssm_name_staging_user_public_key = "${local.ssm_name_prefix}/SFTP_STAGING_USER_PUBLIC_KEY"
-  ssm_name_host_private_key        = "${local.ssm_name_prefix}/SFTP_HOST_PRIVATE_KEY"
-  ssm_name_host_public_key         = "${local.ssm_name_prefix}/SFTP_HOST_PUBLIC_KEY"
-  ssm_name_staging_username        = "${local.ssm_name_prefix}/SFTP_STAGING_USERNAME"
-}
-
-resource "aws_ssm_parameter" "staging_user_public_key" {
-  name        = local.ssm_name_staging_user_public_key
-  type        = "SecureString"
-  value       = "placeholder"
-  description = "AWS Transfer Family only accepts PEM formatted public keys. See https://docs.aws.amazon.com/transfer/latest/userguide/key-management.html#convert-ssh2-public-key"
-  overwrite   = false
-
-  lifecycle {
-    ignore_changes = [
-      value,
-    ]
-  }
-
-  tags = {
-    Project = var.project
-  }
-}
-
-data "aws_ssm_parameter" "staging_user_public_key" {
-  name = local.ssm_name_staging_user_public_key
-  depends_on = [
-    aws_ssm_parameter.staging_user_public_key
-  ]
+  ssm_name_prefix           = "/${var.project}"
+  ssm_name_host_private_key = "${local.ssm_name_prefix}/SFTP_HOST_PRIVATE_KEY"
+  ssm_name_host_public_key  = "${local.ssm_name_prefix}/SFTP_HOST_PUBLIC_KEY"
 }
 
 resource "aws_ssm_parameter" "host_private_key" {
@@ -80,29 +53,3 @@ data "aws_ssm_parameter" "host_public_key" {
     aws_ssm_parameter.host_public_key
   ]
 }
-
-resource "aws_ssm_parameter" "staging_sftp_username" {
-  name        = local.ssm_name_staging_username
-  type        = "SecureString"
-  value       = "staging-username"
-  description = ""
-  overwrite   = false
-
-  lifecycle {
-    ignore_changes = [
-      value,
-    ]
-  }
-
-  tags = {
-    Project = var.project
-  }
-}
-
-data "aws_ssm_parameter" "staging_sftp_username" {
-  name = local.ssm_name_staging_username
-  depends_on = [
-    aws_ssm_parameter.staging_sftp_username
-  ]
-}
-
