3ea942efd6302f3e5ff95bc4c181f1a6c50938ea
Michael Maurer <mjmaurer777@gmail.com>
2024-08-15 11:37:49 -0400

    Add a basic nix shell for developing simple lambdas against boto3

--- shell.nix
+++ shell.nix
@@ -2,5 +2,40 @@ with import <nixpkgs> { };
 
 stdenv.mkDerivation {
   name = "node";
-  buildInputs = [ nodejs-16_x awscli2 ];
+  buildInputs = [ 
+    nodejs-18_x
+    awscli2
+    # Used for lambda type completion:
+    python3
+    python3Packages.pip
+  ];
+
+  shellHook = ''
+    export PYTHONPATH=${pkgs.python3.sitePackages}:$PYTHONPATH
+    python3 -m venv .venv
+    source .venv/bin/activate
+    pip install boto3 boto3-stubs[essential] black
+    source_lambda_env() {
+      while IFS= read -r line; do
+          # Skip empty lines and lines starting with #
+          if [[ ! -z "$line" && ! "$line" =~ ^# ]]; then
+              export "$line"
+          fi
+      done < .env.lambda
+    }
+    lambda_instances() {
+      # Execute in a subshell to avoid polluting the environment
+      (
+        source_lambda_env
+        python terraform/geomatch_app/sagemaker/lambda_source/instances.py
+      )
+    }
+    lambda_access() {
+      # Execute in a subshell to avoid polluting the environment
+      (
+        source_lambda_env
+        python terraform/geomatch_app/sagemaker/lambda_source/access.py
+      )
+    }
+  '';
 }
